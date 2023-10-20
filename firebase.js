b1da31fb412aa753e783d92bb713b6dacf03d8a5
agathaschroeder <124810428+agathaschroeder@users.noreply.github.com>
2023-10-20 16:50:07 -0300

    Create firebase.js

---
+++ firebase.js
@@ -0,0 +1,12 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCRGix-PmoLRx7fD2x3kGWCHACPTnx8KWA",
+  authDomain: "fir-b3bf2.firebaseapp.com",
+  databaseURL: "https://fir-b3bf2-default-rtdb.firebaseio.com",
+  projectId: "fir-b3bf2",
+  storageBucket: "fir-b3bf2.appspot.com",
+  messagingSenderId: "1060541019924",
+  appId: "1:1060541019924:web:836bd0b06453a9b1ddb401",
+  measurementId: "G-HVRYP120NV"
+};
+firebase.initializeApp(firebaseConfig);
